28ac2311c77a94990f122119c9dec8b520689cef
alikiminza <ali.kiminza@student.moringaschool.com>
2023-07-09 10:13:40 +0300

    final changes

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reverseWord = word.toLowerCase().split('').reverse().join('');
+  return word === reverseWord;
 }
 
 /* 
   Add your pseudocode here
 */
-
+ // Create a new word reversing variable (reverseWord)
+  // Take the word and split it into its individual characters.
+  // Use .split as this assigns each character a value.
+  // use .reverse to reverse the order of the characters.
+  // use.join to join the characters back together.
+    // check whether the reversed word is the same as the original
 /*
   Add written explanation of your solution here
 */
@@ -14,12 +21,15 @@ function isPalindrome(word) {
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("abba"));
 
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
 }
 
 module.exports = isPalindrome;
